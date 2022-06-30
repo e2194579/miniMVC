7f77ac9035d8d9a95d07bd5b9ab9460a9d308850
Rbc <e2194579@cmaisonneuve.qc.ca>
2022-06-30 15:27:11 -0400

    MVC v.1

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+const BASE_SERVEUR = '';
+
+const BD_HOTE = 'localhost';
+const BD_UTIL = '';
+const BD_MDP = '';
+const BD_NOM = '';
---
+++ controleurs/NouveauControleur.cls.php
@@ -0,0 +1,13 @@
+<?php
+class NouveauControleur extends Controleur
+{
+    /**
+     * Méthode invoquér par défaut si aucunne action n'est indiquée.
+     */
+    public function index($params)
+    {
+       /*  $this->gabarit->affecterActionParDefaut('tout');
+        $this->tout($params); */
+    }
+
+}
